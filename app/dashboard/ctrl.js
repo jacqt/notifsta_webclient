2fe43108094a67c1b70ce3d3372e7136c1055d4b
Anthony Guo <anthony.guo@some.ox.ac.uk>
2015-05-05 00:20:20 +0100

    made the dashboard look a little nicer

--- app/dashboard/ctrl.js
+++ app/dashboard/ctrl.js
@@ -1,15 +1,23 @@
 /** Anthony Guo (anthony.guo@some.ox.ac.uk)
  * Controller for the dashboard
  */
-(function(){
+(function () {
     angular.module('notifsta.controllers').controller('DashboardCtrl',
-        ['$scope', 'NotifstaHttp', 'toaster' ,function($scope, NotifstaHttp,  toaster, $cookies) {
+        ['$scope', 'NotifstaHttp', 'toaster', function ($scope, NotifstaHttp, toaster, $cookies) {
 
-          $scope.go_create_event = function(){
-            window.location = '#/create_event';
-          }
-          $scope.say_sorry = function(){
-            toaster.pop('info', 'Sorry - we\'re still working on all our features!');
-          }
-    }]);
+
+            $scope.get_date = function(time){
+                return moment(time).format('ll');
+            }
+            $scope.get_time = function(time){
+                return moment(time).format('LT');
+            }
+
+            $scope.go_create_event = function () {
+                window.location = '#/create_event';
+            }
+            $scope.say_sorry = function () {
+                toaster.pop('info', 'Sorry - we\'re still working on all our features!');
+            }
+        }]);
 })();
---
+++ app/util/base/directives.js
@@ -0,0 +1,61 @@
+﻿var app = angular.module('notifsta.directives');
+app.directive('htmlEllipsis', ['$timeout', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            $timeout(function () {
+                angular.element(element).ellipsis();
+            }, 0);
+
+        }
+    };
+}]);
+//For autofocus TODO: move outside of app.js file
+app.directive('focusMe', ['$timeout', function ($timeout) {
+    return {
+        link: function (scope, element, attrs) {
+            scope.$watch(attrs.focusMe, function (value) {
+                if (value === true) {
+                    $timeout(function () {
+                        element[0].focus();
+                        scope[attrs.focusMe] = false;
+                    });
+                }
+            });
+        }
+    };
+}]);
+
+
+app.directive('googlePlaces', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        // transclude:true,
+        scope: { location: '=' },
+        template: '<input id="google_places_ac" name="google_places_ac" type="text" class="input-block-level"/>',
+        link: function ($scope, elm, attrs) {
+            var autocomplete = new google.maps.places.Autocomplete($("#google_places_ac")[0], {});
+            google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                var place = autocomplete.getPlace();
+                $scope.location = place.geometry.location.lat() + ',' + place.geometry.location.lng();
+                $scope.$apply();
+            });
+        }
+    }
+});
+
+//For ease of ng-enter
+app.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if (event.which === 13) {
+                scope.$apply(function () {
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
