3c60083d480f160571b020cbddcbf3533bfd649f
Anthony Guo <anthony.guo@some.ox.ac.uk>
2015-05-16 00:38:30 +0100

    fixed responsiveness of deckgrid again

--- Gruntfile.js
+++ Gruntfile.js
@@ -121,11 +121,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-usemin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', [
-        'sass',
         'useminPrepare',
         'ngtemplates',
         'ngAnnotate',
