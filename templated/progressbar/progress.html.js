3982466df8e76098623565daf3bdf5b1cd113d3b
Anthony Guo <anthony.guo@some.ox.ac.uk>
2015-05-17 02:53:13 +0100

    removed unnecessary files

--- templated/progressbar/progress.html.js
+++
@@ -1,4 +0,0 @@
-angular.module("template/progressbar/progress.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("template/progressbar/progress.html",
-    "<div class=\"progress\" ng-transclude></div>");
-}]);
